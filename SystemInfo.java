a432a2fc7f21f0d6bd1e1fed2162ccabf559b842
md123-dev <rachmanfarell@gmail.com>
2026-05-02 20:32:30 +0700

    SystemInfo.java

---
+++ SystemInfo.java
@@ -0,0 +1,7 @@
+public class SystemInfo {
+    public static void main(String[] args) {
+        System.out.println("Java Runtime Environment Detect...");
+        System.out.println("OS Architecture: " + System.getProperty("os.arch"));
+        System.out.println("Status: Core Java File Loaded.");
+    }
+}
